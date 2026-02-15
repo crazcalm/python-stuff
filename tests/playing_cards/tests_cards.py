8ca98669ae76e92c97f6dc485067e403ae1f6a95
Marcus Willock <crazcalm@gmail.com>
2026-02-15 09:06:06 -0500

    I want this to fail the github action

---
+++ tests/playing_cards/tests_cards.py
@@ -0,0 +1,16 @@
+import unittest
+from unittest import TestCase
+
+from python_stuff.playing_cards.cards import Card
+
+
+class TestCard(TestCase):
+    def test_init(self) -> None:
+        card = Card("2", "suite")
+
+        self.assertEqual(card.value, "2")
+        self.assertEqual(card.suite, "suite")
+
+
+if __name__ == "__main__":
+    unittest.main()
