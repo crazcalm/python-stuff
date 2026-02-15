54ac5e25a96da7ab2007962de73905522e66909d
Marcus Willock <crazcalm@gmail.com>
2026-02-15 09:06:06 -0500

    I want this to fail the github action

---
+++ src/python_stuff/playing_cards/cards.py
@@ -0,0 +1,14 @@
+from typing import ClassVar
+
+class Card:
+    def __init__(self, value: int, suite: str) -> None:
+        self._value = value
+        self._suite = suite
+
+    @property
+    def value(self):
+        return self._value
+
+    @property
+    def suite(self):
+        return self._suite
