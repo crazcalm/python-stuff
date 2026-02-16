2197b109bb3a4175532c0b8173f28693c8e048e0
Marcus Willock <crazcalm@gmail.com>
2026-02-15 22:02:36 -0500

    setup pydoc

--- README.md
+++ README.md
@@ -1,7 +1,23 @@
 # Python_stuff
 I am tired of writing everything form scratch. This will be my library for my personal projects.
 
+## Testing
+### Run with tox
+```
+tox
+```
+
+### Run directly
+```
+python -m unittest discover -v tests/
+```
+
 ## Run Linters
 ```
 tox -e lint
+```
+
+## Run Pydoc
+```
+pydoc -b .
 ```
--- tests/playing_cards/tests_cards.py
+++ tests/playing_cards/tests_cards.py
@@ -1,15 +1,29 @@
 import unittest
 from unittest import TestCase
 
-from python_stuff.playing_cards.cards import Card
+from python_stuff.playing_cards.cards import (
+    Card,
+    CardValue,
+    CardSuite,
+)
 
 
 class TestCard(TestCase):
-    def test_init(self) -> None:
-        card = Card(2, "suite")
+    def setUp(self):
+        card_value = CardValue.TWO
+        card_suite = CardSuite.SPADE
 
-        self.assertEqual(card.value, 2)
-        self.assertEqual(card.suite, "suite")
+        self.card = Card(card_value, card_suite)
+
+    def test_init(self) -> None:
+        self.assertEqual(self.card.value, CardValue.TWO)
+        self.assertEqual(self.card.suite, CardSuite.SPADE)
+
+    def test__repr__(self) -> None:
+        expected = "Card(Two-Spade)"
+        result = str(self.card)
+
+        self.assertEqual(result, expected)
 
 
 if __name__ == "__main__":
