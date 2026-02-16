1c9d30b59f9fed6121958c463b324e6dd43be987
Marcus Willock <crazcalm@gmail.com>
2026-02-15 23:11:30 -0500

    creating cards

--- src/python_stuff/playing_cards/cards.py
+++ src/python_stuff/playing_cards/cards.py
@@ -16,6 +16,19 @@ class CardSuite(StrEnum):
     CLUB = "club"
     JOKER = "joker"
 
+    @classmethod
+    def four_suites(self) -> list[CardSuite]:
+        return [
+            self.SPADE,
+            self.HEART,
+            self.CLUB,
+            self.DIAMOND,
+        ]
+
+    @classmethod
+    def joker_suite(self) -> list[CardSuite]:
+        return [self.JOKER]
+
 
 class CardValue(StrEnum):
     """
@@ -38,6 +51,20 @@ class CardValue(StrEnum):
     BIG_JOKER = "big_joker"
     SMALL_JOKER = "small_joker"
 
+    @classmethod
+    def values_only_jokers(self) -> list[CardValue]:
+        return [self.BIG_JOKER, self.SMALL_JOKER]
+
+    @classmethod
+    def values_no_jokers(self) -> list[CardValue]:
+        exclude = [self.BIG_JOKER, self.SMALL_JOKER]
+
+        return [
+            getattr(self, item)
+            for item in self._member_names_
+            if getattr(self, item) not in exclude
+        ]
+
 
 class Card:
     """
@@ -58,3 +85,30 @@ class Card:
 
     def __repr__(self) -> str:
         return f"Card({self.value.capitalize()}-{self.suite.capitalize()})"
+
+    def __eq__(self, other) -> bool:
+        return self.value == other.value and self.suite == other.suite
+
+
+def create_cards(exclude: list[Card] | None = None) -> list[Card]:
+    results = []
+
+    joker_names = CardValue
+
+    results = [
+        Card(value, suite)
+        for value in CardValue.values_no_jokers()
+        for suite in CardSuite.four_suites()
+    ]
+
+    results += [
+        Card(value, suite)
+        for value in CardValue.values_only_jokers()
+        for suite in CardSuite.joker_suite()
+    ]
+
+    # removing excluded cards
+    if exclude:
+        results = [card for card in results if card not in exclude]
+
+    return results
